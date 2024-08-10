89eed4181a7b6f85e6445c0b5b9e05af76e8977d
Leonardo Breda <leonardo-breda1@hotmail.com>
2024-08-10 18:33:09 -0300

    template, view e url para contato único

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+from django.http import Http404
 
 
 # Create your views here.
@@ -11,3 +12,17 @@ def index(request):
     }
 
     return render(request, 'contact/index.html', context)
+
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    # single_contact = Contact.objects.filter(id=contact_id).first()
+    # if single_contact is None:
+    #     raise Http404()
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(request, 'contact/contact.html', context)
