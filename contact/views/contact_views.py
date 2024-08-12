5ad8822c3e9d90e310b4fe2e144d79d2fd7165bb
Leonardo Breda <leonardo-breda1@hotmail.com>
2024-08-12 18:18:25 -0300

    usando classe Paginator para paginação do index e search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
+from django.core.paginator import Paginator
 from django.http import Http404
 
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
 
@@ -46,10 +51,16 @@ def search(request):
                 Q(email__icontains=search_value))\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
         'search_value': search_value,
     }
 
     return render(request, 'contact/index.html', context)
+
+
