2f8406e927e3ae5f5484f960f37f45f92497d48a
Leonardo Breda <leonardo-breda1@hotmail.com>
2024-08-12 19:47:57 -0300

    criando a URL, view e template para criar contato (CREATE)

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from contact.models import Contact
+from django.core.paginator import Paginator
+from django.http import Http404
+
+
+def create(request):
+    context = {
+
+    }
+
+    return render(request, 'contact/create.html', context)
