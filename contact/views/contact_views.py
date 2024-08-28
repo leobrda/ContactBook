f6523f6a398909d53ba8c0c1b09865f246f63ca4
Leonardo Breda <leonardo-breda1@hotmail.com>
2024-08-27 23:19:37 -0300

    Atualização, user só consegue ver os seus contatos

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -6,18 +6,33 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')
+    if request.user.is_authenticated:
+        contacts = Contact.objects.filter(show=True, owner=request.user).order_by('-id')
 
-    paginator = Paginator(contacts, 10)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+        paginator = Paginator(contacts, 10)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
 
-    context = {
-        'page_obj': page_obj,
-        'site_title': 'Contatos - ',
-    }
+        context = {
+            'page_obj': page_obj,
+            'site_title': 'Contatos - ',
+        }
 
-    return render(request, 'contact/index.html', context)
+        return render(request, 'contact/index.html', context)
+
+    else:
+        contacts = Contact.objects.filter(show=True).order_by('-id')
+
+        paginator = Paginator(contacts, 10)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
+        context = {
+            'page_obj': page_obj,
+            'site_title': 'Contatos - ',
+        }
+
+        return render(request, 'contact/index.html', context)
 
 
 def search(request):
